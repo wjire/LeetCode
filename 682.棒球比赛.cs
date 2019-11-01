f4e4d9260adff87efe66c548f6202c7019aa79d9
wjire <12192900@qq.com>
2019-11-01 15:40:39 +0800

    添加项目文件。

---
+++ 682.棒球比赛.cs
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=682 lang=csharp
+ *
+ * [682] 棒球比赛
+ */
+
+// @lc code=start
+public class Solution {
+  public int CalPoints(string[] ops)
+        {
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < ops.Length; i++)
+            {
+                switch (ops[i])
+                {
+                    case "C":
+                        stack.Pop();
+                        break;
+                    case "D":
+                        bool flag = stack.TryPeek(out int grade);
+                        if (flag)
+                        {
+                            stack.Push(grade * 2);
+                        }
+                        break;
+                    case "+":
+                        bool res = stack.TryPeek(out int g1);
+                        if (res)
+                        {
+                            stack.Pop();
+                            res = stack.TryPeek(out int g2);
+                            int g = g1;
+                            if (res)
+                            {
+                                g = g1 + g2;
+                            }
+                            stack.Push(g1);
+                            stack.Push(g);
+                        }
+                        break;
+                    default:
+                        int n = Convert.ToInt32(ops[i]);
+                        stack.Push(n);
+                        break;
+                }
+            }
+            int result = 0;
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+            return result;
+        }
+}
+// @lc code=end
+
---
+++ 682/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _682
+{
+    public class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < ops.Length; i++)
+            {
+                switch (ops[i])
+                {
+                    case "C":
+                        stack.Pop();
+                        break;
+                    case "D":
+                        bool flag = stack.TryPeek(out int grade);
+                        if (flag)
+                        {
+                            stack.Push(grade * 2);
+                        }
+                        break;
+                    case "+":
+                        bool res = stack.TryPeek(out int g1);
+                        if (res)
+                        {
+                            stack.Pop();
+                            res = stack.TryPeek(out int g2);
+                            int g = g1;
+                            if (res)
+                            {
+                                g = g1 + g2;
+                            }
+                            stack.Push(g1);
+                            stack.Push(g);
+                        }
+                        break;
+                    default:
+                        int n = Convert.ToInt32(ops[i]);
+                        stack.Push(n);
+                        break;
+                }
+            }
+            int result = 0;
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+            return result;
+        }
+    }
+}
